e13d0520581891fed0992ebe96dc458becfc5b7e
Hakim Jurimi <m.hakimjurimi@gmail.com>
2022-09-15 15:02:28 +0800

    Initial commit

---
+++ app/Http/Controllers/StripePaymentController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Stripe;
+use Illuminate\Support\Facades\Session;
+use Stripe\Charge;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class StripePaymentController extends Controller
+{
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stripe()
+    {
+        return view('stripe');
+    }
+
+    /**
+     * success response method.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function stripePost(Request $request)
+    {
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET'));
+        Stripe\Charge::create ([
+                "amount" => 100 * 100,
+                "currency" => "usd",
+                "source" => $request->stripeToken,
+                "description" => "Test payment from itsolutionstuff.com."
+        ]);
+
+        Session::flash('success', 'Payment successful!');
+
+        return back();
+    }
+}
